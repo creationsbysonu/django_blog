4993cfbac882e48dc5ba020f96b81938ae672a68
Sonu <sonu@Sonus-MacBook-Pro.local>
2025-07-06 23:39:25 +0545

    Minor Changes

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import renderRegisterForm,renderLoginForm
+from .views import renderRegisterForm,renderLoginForm,renderHomePage
 
 urlpatterns=[
     path('register',renderRegisterForm),
-    path('login',renderLoginForm)
+    path('login',renderLoginForm),
+    path('',renderHomePage)
 ]
